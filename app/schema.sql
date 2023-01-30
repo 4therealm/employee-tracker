d2bea38f61b8bdc18494db4c1212872c60191d8c
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-30 10:09:37 -0600

    testing

---
+++ app/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE employee_tracker_app;
+USE employee_tracker_app;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal,
+  department_id INT not null AUTO_INCREMENT
+  PRIMARY KEY (id)
+  FOREIGN key (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) not null,
+  role_id int not null
+  manager_id REFERENCES (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
