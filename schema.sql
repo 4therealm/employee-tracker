9d953b76ad742c295fa67bbf776ddb67537aed11
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-30 21:06:18 -0600

    good direction

--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE employee_tracker_app;
-USE employee_tracker_app;
+-- CREATE DATABASE employee_tracker;
+-- USE employee_tracker;
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
 
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary decimal,
-  department_id INT not null AUTO_INCREMENT
-  PRIMARY KEY (id)
-  FOREIGN key (department_id) REFERENCES department(id)
-);
+-- CREATE TABLE department (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   name VARCHAR(255) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE roles (
+--   id INT NOT NULL,
+--   title VARCHAR(30),
+--   salary decimal,
+--   department_id INT,
+--   PRIMARY KEY (id)
+-- );
 
 CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
@@ -22,8 +22,6 @@ CREATE TABLE employees (
   last_name VARCHAR(30) not null,
   role_id int not null,
   manager_id int not null,
-  PRIMARY KEY (id),
-  key(manager_id) REFERENCES (id),
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+  PRIMARY KEY (id)
 );
 
